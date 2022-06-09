8112db444b21731033fb5d269c124c69e6d1382c
Indira Singh <lucindhy@gmail.com>
2022-06-09 19:03:10 -0300

    jetpack: ViewModel e LiveData

--- TodoLista/app/src/main/java/com/generation/todolist/ListFragment.kt
+++ TodoLista/app/src/main/java/com/generation/todolist/ListFragment.kt
@@ -6,24 +6,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.generation.todolist.databinding.FragmentListBinding
+import com.generation.todolist.model.Tarefa
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_list, container, false)
+        binding = FragmentListBinding.inflate(layoutInflater, container, false)
 
-        val floatingAdd = view.findViewById<FloatingActionButton>(R.id.floatingAdd)
+        val listTarefas = listOf(
+            Tarefa(
+                "Ler um livro motivacional",
+                "Ler 2 horas por dia",
+                "Indira",
+                "2022-06-20",
+                false,
+                "Todos os dias"
 
-        floatingAdd.setOnClickListener {
+            ),
+            Tarefa(
+                "Fazer curso de programação",
+                "Pelo menos 15h por semana",
+                "Indira",
+                "2022-04-05",
+                true,
+                "3 vezes por semana"
+
+            ),
+            Tarefa(
+                "Fazer atividade fisica",
+                "Qualquer tipo de atividade ao ar livre",
+                "Indira",
+                "2022-01-01",
+                true,
+                "Diariamente"
+            )
+        )
+
+        binding.floatingAdd.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_formFragment)
         }
 
-        return view
+        return binding.root
     }
 
 }
---
+++ TodoLista/app/src/main/java/com/generation/todolist/adapter/TarefaAdapter.kt
@@ -0,0 +1,4 @@
+package com.generation.todolist.adapter
+
+class TarefaAdapter {
+}
